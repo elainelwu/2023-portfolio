7594a01e6b8941ce836897232dc1ff130ea256fb
Elaine Wu <elainewu304@gmail.com>
2024-01-10 02:14:21 -0500

    fixed apostrophe

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <div className="flex flex-col place-self-center">
-        <h1 className="text-8xl">Hi! I'm Elaine Wu,</h1>
+        <h1 className="text-8xl">Hi! I&apos;m Elaine Wu,</h1>
         <p className="max-w-4xl text-4xl text-wrap">an aspiring full-stack developer studying computer science at Cornell University.</p>
       </div>
     </main>
