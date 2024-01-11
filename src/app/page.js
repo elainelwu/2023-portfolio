bbd8930eb4f15fe3a1b0801b0c4ea72a8445778a
Elaine Wu <elainewu304@gmail.com>
2024-01-11 12:03:52 -0500

    added aurora ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import styles from '@/app/ui/aurora.module.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <div className="flex flex-col place-self-center">
+    <main className="flex flex-col items-center">
+      <div className={styles.container}>
+        <div className={`${styles.base} ${styles.one}`}></div>
+        <div className={`${styles.base} ${styles.two}`}></div>
+        <div className={`${styles.base} ${styles.three}`}></div>
+        <div className={`${styles.base} ${styles.four}`}></div>
+      </div>
+      <div className="flex flex-col h-screen justify-center absolute">
         <h1 className="text-8xl">Hi! I&apos;m Elaine Wu,</h1>
         <p className="max-w-4xl text-4xl text-wrap">an aspiring full-stack developer studying computer science at Cornell University.</p>
       </div>
