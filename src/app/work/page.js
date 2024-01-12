87b581f51140d3108ee611af27c28e948650c845
Elaine Wu <elainewu304@gmail.com>
2024-01-11 19:37:48 -0500

    created about and work pages

---
+++ src/app/work/page.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import Link from 'next/link';
+import auroraStyles from '@/app/ui/aurora.module.css';
+
+export default function Work() {
+  return (
+    <main className="flex flex-col items-center">
+      <div className={auroraStyles.container}>
+        <div className={`${auroraStyles.base} ${auroraStyles.nine}`}></div>
+        <div className={`${auroraStyles.base} ${auroraStyles.ten}`}></div>
+        <div className={`${auroraStyles.base} ${auroraStyles.eleven}`}></div>
+        <div className={`${auroraStyles.base} ${auroraStyles.twelve}`}></div>
+      </div>
+      <div className="flex flex-col h-screen w-screen absolute font-semibold">
+        <nav className="flex flex-row w-full justify-between px-6 pl-10 pr-12 py-6 z-20">
+          <Link href="/">
+            <Image src="/nav-logo.svg"
+              width={50}
+              height={50}
+              className="hover:invert hover:opacity-75 transition ease-in-out delay-100"
+            />
+          </Link>
+          <ul className="flex flex-row text-white space-x-20">
+            <li className="place-self-center hover:invert hover:opacity-75 transition ease-in-out delay-100"><Link href="/">Home</Link></li>
+            <li className="place-self-center hover:invert hover:opacity-75 transition ease-in-out delay-100"><Link href="/about">About</Link></li>
+            <li className="place-self-center hover:invert hover:opacity-75 transition ease-in-out delay-100"><Link href="/work">Work</Link></li>
+          </ul>
+        </nav>
+        <div className="flex items-center justify-center h-screen w-screen absolute">
+          <p className="text-4xl">coming soon...</p>
+        </div>
+      </div>
+    </main>
+  )
+}
