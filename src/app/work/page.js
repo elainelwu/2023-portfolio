711cc1b5549e2fa22c322c95f8fe27ca3f0137ac
Elaine Wu <elainewu304@gmail.com>
2024-01-19 20:30:10 -0500

    basic implementation of work page

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -1,14 +1,20 @@
 import Aurora from '@/app/ui/aurora.js'
 import NavBar from '@/app/ui/nav-bar.js'
+import Experiences from '@/app/ui/experiences.js'
+import Projects from '@/app/ui/projects.js'
 
 export default function Work() {
   return (
     <main className="flex flex-col items-center">
       <Aurora page={'work'} />
-      <div className="flex flex-col h-screen w-screen absolute font-semibold">
+      <div className="flex flex-col min-h-screen w-screen absolute font-semibold">
         <NavBar />
-        <div className="flex items-center justify-center h-screen w-screen absolute">
-          <p className="text-4xl">coming soon...</p>
+        <div className="flex flex-col items-center h-full w-full relative mb-24">
+          <h1 className="text-6xl">Work</h1>
+          <hr className="w-24 h-1 mx-auto my-8 bg-white border-0 rounded" />
+          <Projects />
+          <hr className="w-24 h-1 mx-auto my-8 bg-white border-0 rounded" />
+          <Experiences />
         </div>
       </div>
     </main>
