3632b0ff743b19ad5ad66977889074689aac6c31
Elaine Wu <elainewu304@gmail.com>
2024-01-19 21:25:38 -0500

    fixed apostrophes

--- src/app/ui/experiences.js
+++ src/app/ui/experiences.js
@@ -9,7 +9,7 @@ export default function Experiences() {
           <h3 className="text-2xl">Cornell University</h3>
           <h4 className="text-xl">Undergraduate Teaching Assistant (TA)</h4>
           <ul className="ml-6 list-disc font-normal">
-            <li>Assisted CS 2800: Discrete Structures throughout Fall '23 and Spring '24 semesters.</li>
+            <li>Assisted CS 2800: Discrete Structures throughout Fall &apos;23 and Spring &apos;24 semesters.</li>
             <li>Lead weekly discussion section of 25 students, facilitate active participation, address questions, and guide engaging discussion on challenging topics in discrete mathematics.</li>
             <li>Provide individualized support to students during office hours, offer guidance on problem-solving strategies, and clarify course concepts.</li>
             <li>Grade assignments and exams promptly to provide constructive feedback to students to help them understand their mistakes and improve their performance.</li>
@@ -21,10 +21,10 @@ export default function Experiences() {
           <h3 className="text-2xl hover:invert hover:opacity-75 transition ease-in-out delay-100"><Link href="https://www.cornellnexus.com/">Cornell Nexus Project Team</Link></h3>
           <h4 className="text-xl">Business/Design Sub-team Member</h4>
           <ul className="ml-6 list-disc font-normal">
-            <li>Lead an overhaul of the project team's website, implementing a user-centered design approach resulting in improved functionality and aesthetics.</li>
+            <li>Lead an overhaul of the project team&apos;s website, implementing a user-centered design approach resulting in improved functionality and aesthetics.</li>
             <li>Develop high-fidelity prototypes using Figma, contributing to the enhancement of project deliverables and facilitating efficient collaboration within the team.</li>
-            <li>Direct, film, and edit compelling promotional videos that effectively highlighted the team's achievements and initiatives, garnering increased engagement and recognition.</li>
-            <li>Produce captivating graphics and strategically managed social media content, fostering a cohesive online presence and amplifying the team's visibility across multiple platforms.</li>
+            <li>Direct, film, and edit compelling promotional videos that effectively highlighted the team&apos;s achievements and initiatives, garnering increased engagement and recognition.</li>
+            <li>Produce captivating graphics and strategically managed social media content, fostering a cohesive online presence and amplifying the team&apos;s visibility across multiple platforms.</li>
           </ul>
         </div>
       </div>
