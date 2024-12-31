676e3f7c1c41025c2b956240fb34cf08afd9c090
Elaine Wu <ew457@cornell.edu>
2024-12-30 22:42:17 -0500

    update school info

--- src/app/ui/about.js
+++ src/app/ui/about.js
@@ -5,8 +5,8 @@ export default function AboutContent() {
   return (
     <div className="flex flex-row max-w-screen space-x-8">
       <div className="flex flex-col max-w-2xl space-y-4 text-xl font-normal">
-        <p>Hello, I&apos;m Elaine! I am currently a sophomore enrolled in Cornell University’s College of Engineering and I am studying computer science with a minor in information science. My goals lie in creating detailed, meaningful, and functional applications.</p>
-        <p>My interest in computer science originally started in middle school when I learned how to customize Tumblr blog pages (you surprisingly learn a lot about HTML and CSS in the process). Now in college, I&apos;ve expanded that budding interest into passion for creating large-scale applications using the valuable tool-kits and knowledge I have gained from mentors, peers, and the Internet! Aside from coding, I am also ardent about the design process, and I dabble a bit in UI/UX design.</p>
+        <p>Hello, I&apos;m Elaine! I am currently a junior enrolled in Cornell University’s College of Engineering and I am studying computer science with a minor in psychology. My goals lie in creating detailed, meaningful, and functional applications.</p>
+        <p>My interest in computer science originally started in middle school when I learned how to customize Tumblr blog pages (you surprisingly learn a lot about HTML and CSS in the process). Now in college, I&apos;ve expanded that budding interest into passion for creating large-scale applications using the valuable tool-kits and knowledge I have gained from mentors, peers, and the Internet! Aside from coding, I am also ardent about the design process, and I dabble a bit in UI/UX design. (last updated: 12/30/24)</p>
       </div>
       <div className="flex flex-col items-center justify-center space-y-4">
         <Image src="/headshot.JPG"
